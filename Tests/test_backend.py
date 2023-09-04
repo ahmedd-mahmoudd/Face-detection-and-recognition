f88ca0e30daea9b6df0fb1e21a70ca4a9efcdea0
Ahmed Mahmoud <mahmouda091.am@gmail.com>
2023-09-04 05:44:06 +0300

    changing the backend folder name

---
+++ Tests/test_backend.py
@@ -0,0 +1,37 @@
+import unittest
+import json
+from ..backend_server.main import app 
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        # Create a test client for the app
+        self.app = app.test_client()
+
+    def test_test_route(self):
+        # Test the '/test' route
+        response = self.app.get('/test')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['status'], 'Server is up')
+
+    def test_signup_route(self):
+        # Test the '/signup' route
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.app.post('/signup', json=data)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data['message'], 'User registered successfully.')
+
+    def test_signin_route(self):
+        # Test the '/signin' route
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.app.post('/signin', json=data)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('access_token' in data)
+
+    # Add more test cases for other routes as needed
+
+if __name__ == '__main__':
+    unittest.main()
